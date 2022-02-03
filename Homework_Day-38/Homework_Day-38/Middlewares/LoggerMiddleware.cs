b79364f8dc29bd513640358fffcb104e4810d47f
Davit Jimshiashvili <davit.jimshiashvili698@ens.tsu.edu.ge>
2022-02-04 03:26:22 +0400

    finished Homework Day-38

---
+++ Homework_Day-38/Homework_Day-38/Middlewares/LoggerMiddleware.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace Homework_Day_38.Middlewares
+{
+    public class LoggerMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly ILogger<WeatherForecast> _logger;
+
+        public LoggerMiddleware(RequestDelegate next, ILogger<WeatherForecast> logger)
+        {
+            _logger = logger;
+            _next = next;
+        }
+
+        public async Task InvokeAsync(HttpContext httpContext)
+        {
+            try
+            {
+                await _next(httpContext);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Something went wrong: {ex}");
+            }
+            finally
+            {
+                var statusCode = httpContext.Response.StatusCode;
+
+                if (statusCode ==200)
+                {
+                    _logger.LogInformation("proccess is going without errors");
+                }
+                else if (statusCode >= 400 && statusCode < 500)
+                {
+                    _logger.LogWarning("warning");
+                }
+                else if (statusCode >= 500)
+                {
+                    _logger.LogError("error occured");
+                }
+            }
+        }
+
+      
+    }
+}
---
+++ Homework_Day-38/Homework_Day-38/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Homework_Day_38
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
